8362d6d152b9e1b69da0087d59dd2ed4e23728f2
Prakhar Agarwal(pa19) <pa19@illinois.edu>
2026-04-28 20:47:39 -0500

    adding the 4th query

--- queries.py
+++ queries.py
@@ -6,6 +6,36 @@ MONGO = os.getenv("MONGO", MONGO_URL)
 client = MongoClient(MONGO, serverSelectionTimeoutMS=5000)
 db = client.twitter_project
 tweets = db.tweets
+def engagement_breakdown(limit=None):
+    pipeline = [
+        {"$match": {"user.verified": True}},
+        {"$group": {
+            "_id": "$user.id",
+            "user_name":   {"$first": "$user.name"},
+            "screen_name": {"$first": "$user.screen_name"},
+            "total":   {"$sum": 1},
+            "retweets":{"$sum": {"$cond": [{"$eq": ["$tweet_type", "retweet"]}, 1, 0]}},
+            "quotes":  {"$sum": {"$cond": [{"$eq": ["$tweet_type", "quote"]},   1, 0]}},
+            "replies": {"$sum": {"$cond": [{"$eq": ["$tweet_type", "reply"]},   1, 0]}},
+            "simples": {"$sum": {"$cond": [{"$eq": ["$tweet_type", "tweet"]},   1, 0]}},
+        }},
+        {"$project": {
+            "_id": 0,
+            "user_name": 1,
+            "screen_name": 1,
+            "total_tweets": "$total",
+            "simple_percent":  {"$multiply": [{"$divide": ["$simples",  "$total"]}, 100]},
+            "retweet_percent": {"$multiply": [{"$divide": ["$retweets", "$total"]}, 100]},
+            "quote_percent":   {"$multiply": [{"$divide": ["$quotes",   "$total"]}, 100]},
+            "reply_percent":   {"$multiply": [{"$divide": ["$replies",  "$total"]}, 100]},
+        }},
+        {"$sort": {"total_tweets": -1}}
+    ]
+    if limit:
+        pipeline.append({"$limit": limit})
+    return list(tweets.aggregate(pipeline))
+
+
 
 def query_top_country():
     pipeline = [
